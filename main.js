45d6f1715ffbbcf032b6ea819e31bbb29b22fe1b
Leonardo Wiese <wiese.programmer@gmail.com>
2023-12-14 14:09:36 -0300

    entrega

--- main.js
+++ main.js
@@ -1,14 +1,18 @@
-function adjustStyles() {
-      const screenWidth = window.innerWidth;
+function updateBackgroundColors() {
+    const screenWidth = window.innerWidth;
 
-      if (screenWidth <= 640) {
-        document.body.classList.add('mobile');
-      } else if (screenWidth <= 768) {
-        document.body.classList.add('tablet');
-      } else {
-        document.body.classList.remove('mobile', 'tablet');
-      }
+    // Check if the screen width is less than or equal to 450px
+    if (screenWidth <= 450) {
+      // Apply responsive background colors
+      document.body.style.backgroundColor = 'new_color_here';
+      // You can update other elements' background colors in a similar way
+    } else {
+      // Revert to the original background colors
+      document.body.style.backgroundColor = 'original_color_here';
+      // Reset other elements' background colors
     }
+  }
 
-    window.onload = adjustStyles;
-    window.onresize = adjustStyles;
+  // Call the function on page load and window resize
+  updateBackgroundColors();
+  window.addEventListener('resize', updateBackgroundColors);
